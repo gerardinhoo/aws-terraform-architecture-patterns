fa7d291607f30aa5a2dafc17dab152a938ac1f01
Gerard <gerard.eklu@yahoo.fr>
2026-04-02 00:27:44 -0400

    node_modules/ app/node_modules/ .env npm-debug.log .DS_Store

---
+++ ecs-node/app/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.get("/", (req, res) => {
+    res.send( "Hello from ECS Fargate");
+})
+
+app.get("/health", (req, res) => {
+    res.status(200).json({status: "OK"});
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
