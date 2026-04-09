d3a9d95a454266fd66c9dad25d1ac7628a270b57
Gerard <gerard.eklu@yahoo.fr>
2026-04-08 20:03:03 -0400

    ci cd test

--- lambda-task-api/app/create-task/index.js
+++ lambda-task-api/app/create-task/index.js
@@ -21,7 +21,7 @@ exports.handler = async (event) => {
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: "Task saved",
+        message: "Testing ci cd",
         task
       })
     };
